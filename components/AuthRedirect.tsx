2e44a22b7f6f271a71184e5ed5a963c8d8e4c404
Saba Saif <saba.saif@relexsolutions.com>
2023-06-04 19:57:54 +0300

    Styled new post modal

---
+++ components/AuthRedirect.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Link from "next/link";
+
+const AuthRedirect = () => {
+  return (
+    <main className="flex h-screen w-screen justify-center flex-col gap-3  items-center">
+      <div>You're already logged in!</div>
+      <div className="flex justify-between gap-3">
+        <Link
+          className=" border-2 rounded hover:bg-black hover:text-white dark:hover:bg-gray-50 dark:hover:text-gray-900 p-2"
+          href="/"
+        >
+          Home
+        </Link>
+      </div>
+    </main>
+  );
+};
+
+export default AuthRedirect;
